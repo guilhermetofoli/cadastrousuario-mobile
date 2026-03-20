782d64cc64070ad4522f7f00afb9cc4c1e0c1a1f
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-03-19 21:51:56 -0300

    Criado arquivo, com funções nos botões

---
+++ app/src/main/java/com/tofoli/cadastrousuario/CreateUser.java
@@ -0,0 +1,37 @@
+package com.tofoli.cadastrousuario;
+
+//Bloco de importações
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+//Classe principal
+public class CreateUser extends AppCompatActivity {
+    TextInputEditText editNome, editEmail;
+    Button btnSalvar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_user);
+
+        // Vincula variável com elemento da interface gráfica, escrito no arquivo xml
+        editNome = findViewById(R.id.nome_completo);
+        editEmail = findViewById(R.id.email);
+        btnSalvar = findViewById(R.id.btnSalvar);
+
+        btnSalvar.setOnClickListener(v -> {
+            String nome = editNome.getText().toString();
+            String email = editEmail.getText().toString();
+
+            if (!nome.isEmpty()){
+                MainActivity.listaNomes.add(nome + " (" + email + ") ");
+                finish();
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
@@ -0,0 +1,4 @@
+package com.tofoli.cadastrousuario;
+
+public class UserAdapter {
+}
