f0de7ecf79ea93aa64d41b4904cf313e6ace9394
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-03-26 19:24:15 -0300

    Inserido o RecyclerView na UserAdapter.java

--- app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
+++ app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
@@ -1,4 +1,8 @@
 package com.tofoli.cadastrousuario;
 
-public class UserAdapter {
+import androidx.recyclerview.widget.RecyclerView;
+
+//Classe Adapter: Gerencia a criação e o preenchimento dos itens na RecycleView
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
 }
