2e182a7f65d7bbf5a059f505a0eafa9ade4c4b60
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-03-26 19:29:14 -0300

    Criado o construtor que entrega os dados ao Adapter

--- app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
+++ app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
@@ -2,7 +2,16 @@ package com.tofoli.cadastrousuario;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 //Classe Adapter: Gerencia a criação e o preenchimento dos itens na RecycleView
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+    //Atributo privado que armazena a referência dos dados que serão exibidos
+    private List<String> ListaUsuarios;
+
+    //Construtor que permite que a MainActivity "entregue" a lista de dados para este Adapter
+    public UserAdapter(List<String> lista){
+
+    }
 
 }
