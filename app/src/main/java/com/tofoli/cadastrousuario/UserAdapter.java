75719257dbb8477c3515b2e6f7cb4016563f880c
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-03-26 19:38:10 -0300

    Criado método para visual linha a linha

--- app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
+++ app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
@@ -1,16 +1,29 @@
 package com.tofoli.cadastrousuario;
 
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
 //Classe Adapter: Gerencia a criação e o preenchimento dos itens na RecycleView
 public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     //Atributo privado que armazena a referência dos dados que serão exibidos
-    private List<String> ListaUsuarios;
+    private List<String> listaUsuarios;
 
     //Construtor que permite que a MainActivity "entregue" a lista de dados para este Adapter
     public UserAdapter(List<String> lista){
+        this.listaUsuarios = lista;
+    }
+
+    //Método 1: Cria "do zero" o visual de uma linha da linha (+ViewHolder)
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
+
 
     }
 
