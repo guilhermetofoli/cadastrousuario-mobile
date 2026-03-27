1e005a668373074bf843546616a24e44bb3472b0
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-03-26 21:03:25 -0300

    Cirreção no método 2: Retirada do RecyclerView

--- app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
+++ app/src/main/java/com/tofoli/cadastrousuario/UserAdapter.java
@@ -35,7 +35,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
 
     //Método 2: Vincula os dados de um objeto de lista a uma linha específica da tela
     @Override
-    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position){
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position){
         //Recupera os dados da lista de acordo com a posição que o Android está desenhando agora
         String nome = listaUsuarios.get(position);
 
@@ -60,7 +60,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             super(itemView);
             //Faz o mapeamento do ID do layout para o objeto JAVA
             //android.R.id.text1 é o ID padrão do layout 'simple_list_item_1'
+            tvNome = itemView.findViewById(android.R.id.text1);
         }
-
     }
 }
